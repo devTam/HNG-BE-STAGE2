7c6a9b2f3e8d94608293c4b91450e931beb24f7a
devTam <tammy.batubo@gmail.com>
2021-08-20 01:04:43 +0100

    add contact form

---
+++ client/app.js
@@ -0,0 +1,49 @@
+const formName = document.getElementById('form-name')
+const email = document.getElementById('form-email')
+const comment = document.getElementById('form-comment')
+const submit = document.getElementById('submit')
+const form = document.getElementById('contact-form');
+const response = document.getElementById('response');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const formData = {
+        name: formName.value,
+        email: email.value,
+        comment: comment.value
+    }
+
+    try {
+        const data = await fetch("/", {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(formData),
+        });
+
+        const result = await data.json();
+
+        submit.disabled = true;
+        response.innerText = result;
+
+        // setTimeout(() => {
+        //     submit.disabled = false;
+        //     response.innerText = ""
+        // }, 2000)
+    
+        formName.value = ""
+        email.value = ""
+        comment.value = ""
+    } catch (error) {
+        console.log(error)
+    }
+
+    
+
+
+})
+
+const printName = (name) => console.log(`My name is ${name}`);
+printName('Tammy Batubo');
---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import nodemailer from "nodemailer";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.static('client'))
+app.use(express.json());
+
+app.get("/", (_, res) => {
+    res.sendFile(__dirname + '/client/index.html')
+});
+
+app.post("/", (req, res) => {
+    const { body: { name, email, comment } } = req;
+  
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "bigmikehng@gmail.com",
+            pass: "bigmike101@"
+        }
+    });
+
+    const mailOptions = {
+        to: "bigmikehng@gmail.com",
+        from: email,
+        subject: `message from ${name} with email: ${email}`,
+        text: comment
+    }
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if(err) {
+            console.log(err)
+            res.json('Oops failed to send, Please try again')
+        }else {
+            res.json('Your message has been sent successfully');
+        }
+
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
